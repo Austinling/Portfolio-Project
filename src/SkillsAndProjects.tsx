a1e2152e64fab59f9bc8c2beffcd5afb3ef48ac6
Austinling <94096647+Austinling@users.noreply.github.com>
2026-02-11 14:24:55 +0000

    Added a new project

--- src/SkillsAndProjects.tsx
+++ src/SkillsAndProjects.tsx
@@ -114,7 +114,7 @@ export function SkillsAndProjects() {
                   </h1>
                 </div>
 
-                <div className="flex">
+                <div className="grid grid-cols-3 gap-10">
                   {Object.entries(project.technologies).map(([key, value]) => {
                     return (
                       <div className="flex gap-3">
